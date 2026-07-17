eb7fc7413eff049cbf632e0f14a86abaff48c470
lihuan <745917886@qq.com>
2026-07-17 14:35:24 +0800

    A方案: Windows 启用 oatpp-openssl (TLS/HTTPS) 支持
    - 启用 deps/oatpp-openssl 子工程构建
    - sfu 链接 oatpp-openssl 库
    - app_component.hpp 移除 #ifndef _WIN32 条件编译
    - TLS 握手测试通过（TLSv1.3 / TLS_AES_256_GCM_SHA384）

--- sfu/src/app_component.hpp
+++ sfu/src/app_component.hpp
@@ -15,9 +15,7 @@
 #include "dto/config.hpp"
 #include "utils/statistics.hpp"
 #include "oatpp/Environment.hpp"
-#ifndef _WIN32
 #include "oatpp-openssl/server/ConnectionProvider.hpp"
-#endif
 #include "oatpp/web/server/interceptor/RequestInterceptor.hpp"
 #include "oatpp/web/server/AsyncHttpConnectionHandler.hpp"
 #include "oatpp/web/server/HttpRouter.hpp"
@@ -125,8 +123,7 @@ public:
 
         std::shared_ptr<oatpp::network::ServerConnectionProvider> result;
 
-        // TODO: use valid cert
-#ifndef _WIN32
+        // A方案: Windows 上启用 TLS 支持
         if (appConfig->useTLS) {
             OATPP_LOGd("oatpp::openssl::Config", "key_path='%s'", appConfig->tlsPrivateKeyPath->c_str());
             OATPP_LOGd("oatpp::openssl::Config", "chn_path='%s'", appConfig->tlsCertificateChainPath->c_str());
@@ -137,10 +134,6 @@ public:
         } else {
             result = oatpp::network::tcp::server::ConnectionProvider::createShared({"0.0.0.0", appConfig->port, oatpp::network::Address::IP_4});
         }
-#else
-        // TLS not supported on Windows debug builds (oatpp-openssl requires pre-built OpenSSL)
-        result = oatpp::network::tcp::server::ConnectionProvider::createShared({"0.0.0.0", appConfig->port, oatpp::network::Address::IP_4});
-#endif
 
         return result;
     }());
